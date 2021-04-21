f5c17d760d86df05155403f545e0435bbbbc2cc2
Joaquin <joaquinbriz1@gmail.com>
2021-04-21 17:09:05 -0300

    products Styles

--- src/components/Product/Products.js
+++ src/components/Product/Products.js
@@ -1,44 +1,58 @@
 import './ProductStyles.css';
+import SupremeSweatshirt from './img/supreme.jpg';
+import BalenciagaHoodie from './img/balenciagahoodie.jpg';
+import CommeDesGarçonsPlayPolo from './img/CommeDesGarçonsPlay.jpg';
+import PoloRalphLauren from './img/PoloRalphLauren.jpg';
+import TheNorthFacexSupreme from './img/TheNorthFacexSupreme.jpg';
 
 export const Products = () => {
   const products =  [
     {
-      image:'img.jpg',
-      name:'Product 1',
-      price:'2021'
+      image: SupremeSweatshirt,
+      name:'Sweatshirt Supreme',
+      price:'$ 1,320'
     },
     {
-      image:'img.jpg',
-      name: 'Product 2',
-      price: '2022'
+      image: BalenciagaHoodie,
+      name: 'Hoodie Balenciaga',
+      price: '$ 895'
     },
     {
-      image:'img.jpg',
-      name: 'Product 2',
-      price: '2022'
+      image: CommeDesGarçonsPlayPolo,
+      name: 'Comme Des Garçons Play',
+      price: '$ 152'
     },
     {
-      image:'img.jpg',
-      name: 'Product 2',
-      price: '2022'
+      image: PoloRalphLauren,
+      name: 'Polo Ralph Lauren',
+      price: '$ 864'
     },
     {
-      image:'img.jpg',
-      name: 'Product 2',
-      price: '2022'
+      image: TheNorthFacexSupreme,
+      name: 'The North Face X Supreme',
+      price: '$ 2,231'
     }
   ]
+  function buying (){
+    alert('Thanks for buy');
+  }
   return(
     <div className='productsContainer'>
-    <ul>
-      {products.map((product) => 
-          <li className='product'>
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
-            <button>Buy</button>
-          </li>
-      )}
-    </ul>
+      <div className='newCategory'>
+        <h2 className='newTitle'>New Releases</h2>
+        <p className='newDescription'>Collaborations and <span>exclusive</span> pieces</p>
+      </div>
+      <ul>
+        {products.map((product) => 
+            <li className='product'>
+            <img src={product.image} alt=''></img>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+              <input className='incrementOrder' type="number" step="1" placeholder='0' />
+              <button onClick={buying} className='buttonBuy'>Buy</button>
+            </li>
+        )}
+      </ul>
     </div>
   )
 }
--- src/components/carousel/CarouselSlider.js
+++ src/components/carousel/CarouselSlider.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, {useState} from 'react';
 import { Carousel } from './Carousel';
 import './CarouselSlides.css';
